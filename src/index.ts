58e89fa0f08df3c9af6bd742fdc0167edc2de297
MamedYahyayev <yahyayev2001@gmail.com>
2023-04-21 18:15:34 +0400

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,16 @@
 import { TextComponent } from "./components/textComponent";
-import { HtmlEvent } from "./model/htmlEvent";
 import { HtmlTag } from "./model/htmlTag";
 import { Editor } from "./tree/editor";
 
 // DOM Elements
-const editorEl = document.getElementById("editor") as HTMLDivElement;
 const insertTextElementBtn = document.getElementById(
   "insert-text-btn"
 ) as HTMLButtonElement;
 
-const paragraph = document.querySelector(".paragraph") as HTMLParagraphElement;
-
-// insertTextElementBtn.addEventListener("click", () => {
-//   const textComponent = new TextComponent(HtmlTag.P);
-//   const textElement = textComponent.getHtmlElement();
-//   editorEl.appendChild(textElement);
-// });
-
-// function addTextIntoEditor() {
-//   const textComponent = new TextComponent(HtmlTag.P, addTextIntoEditor);
-//   const textElement = textComponent.getHtmlElement();
-//   editorEl.appendChild(textElement);
-//   textElement.focus();
-// }
-
 function start(): void {
   const editor = new Editor("editor");
+  const textComponent = new TextComponent(HtmlTag.P);
+  editor.addComponent(textComponent);
 }
+
+start();

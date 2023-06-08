06298c3877cb9a3386d93ffcabb9b77cfc575739
MamedYahyayev <yahyayev2001@gmail.com>
2023-06-08 11:27:28 +0400

    Change Logger printing style

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,20 +2,16 @@ import { mode } from "../config/constants";
 
 declare global {
   interface Logger {
-    debug(...params: any[]): void;
-    debug(message: string, ...params: any[]): void;
+    debug(message?: string, ...params: any[]): void;
   }
 }
 
 export {};
 
 class DefaultLogger implements Logger {
-  debug(...params: any[]): void;
-  debug(message: string, ...params: any[]): void;
-  debug(...args: any[]): void {
-    const [message, ...params] = args;
+  debug(message?: string, ...params: any[]): void {
     if (mode == "dev") {
-      console.log(`%cDEBUG: ${message}`, "color: #FF4500", params);
+      console.log(`%cDEBUG: ${message}`, "color: #FF4500", ...params);
     }
   }
 }
